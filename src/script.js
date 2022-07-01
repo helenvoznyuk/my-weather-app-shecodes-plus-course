00cea568680e53e3a1501c7f87c36f84c30143b0
Helen <vznkhelen@gmail.com>
2022-07-02 00:17:40 +0300

    Added humidity and wind speed, fixed layout for top right card

--- src/script.js
+++ src/script.js
@@ -32,34 +32,6 @@ function displayDayTime() {
   spanDate.innerHTML = correctDateFormat;
 }
 
-// current weather button
-// function getPosition(position) {
-//   let apiKey = "d79bb097b2e1cbd47665f848849e6265";
-//   let latitude = position.coords.latitude;
-//   let longitude = position.coords.longitude;
-//   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
-//   axios.get(url).then(myCurrentWeather);
-// }
-
-// function myCurrentWeather(response) {
-//   let detectedTemp = Math.round(response.data.main.temp);
-//   let correctTemp = document.querySelector("#main-temperature");
-//   correctTemp.innerHTML = `${detectedTemp}`;
-
-//   let displayedLocation = document.querySelector("#entered-location");
-//   displayedLocation.innerHTML = "Your current location";
-
-//   let displayedCondition = document.querySelector("#weather-condition");
-//   displayedCondition.innerHTML = response.data.weather[0].main;
-// }
-
-// function getCurrentPosition() {
-//   navigator.geolocation.getCurrentPosition(getPosition);
-// }
-
-//submitting location name via clicking 'Search' button next to the input field
-// +
-//allowing to enter the city -> displaying its name and current temperature
 function showWeather(response) {
   let enteredTemp = Math.round(response.data.main.temp);
   let correctTemp = document.querySelector("#main-temperature");
@@ -76,7 +48,13 @@ function showWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
 function showLocation(event) {
   event.preventDefault();
   let locationInput = document.querySelector("#city-input");
@@ -93,11 +71,6 @@ function showLocation(event) {
 
   axios.get(apiUrl).then(showWeather);
 }
-
-let form = document.querySelector("#city-input-form");
-form.addEventListener("submit", showLocation);
-
-// //converting temperature value
 
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
@@ -126,25 +99,7 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-// function convertToCelsius(event) {
-//   event.preventDefault();
-//   let celsiusValueDisplayed = document.querySelector("#main-temperature");
-//   celsiusValueDisplayed.innerHTML = 22;
-// }
-
-// function convertToFahrenheit(event) {
-//   event.preventDefault();
-//   let fahrenheitValueDisplayed = document.querySelector("#main-temperature");
-//   fahrenheitValueDisplayed.innerHTML = 71.6;
-// }
-
-// let celsiusValue = document.querySelector("#celsius-link");
-// celsiusValue.addEventListener("click", convertToCelsius);
-
-// let fahrenheitValue = document.querySelector("#fahrenheit-link");
-// fahrenheitValue.addEventListener("click", convertToFahrenheit);
-
-// let updateButton = document.querySelector("#current-city-input-button");
-// updateButton.addEventListener("click", getCurrentPosition);
+let form = document.querySelector("#city-input-form");
+form.addEventListener("submit", showLocation);
 
 displayDayTime();
